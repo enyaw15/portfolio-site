2fb1a0e347c468a8251ecbcdbccd0be7510121bf
enyaw15 <enyaw0215@gmail.com>
2022-07-13 22:16:30 -0700

    added sub app for which will be my portfolio

---
+++ portfolio/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    #the empty string is the URL
+    #the views.index is the function in in views.py to go to
+    #name is the tab name(if you hover over the tab in google chrome it will say the name)
+    path('', views.index, name='index'),
+]
